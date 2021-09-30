c1fed975cbb6586337b89df2b4883a917e22ab42
benjahauge <Benjahauge@hotmail.com>
2021-09-30 10:29:10 +0200

    Add project files.

---
+++ Controllers/ShoesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OblRestExcercise.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using OblRestExcercise.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OblRestExcercise.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ShoesController : ControllerBase
+	{
+		private readonly ShoesManager _manager = new ShoesManager();
+
+		// GET: api/<ShoesController>
+		[HttpGet]
+		public IEnumerable<Shoe> Get()
+		{
+			return _manager.GetAll();
+		}
+
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		// GET api/<ShoesController>/5
+		[HttpGet("{id}")]
+		public ActionResult<Shoe> Get(int id)
+		{
+			Shoe shoe = _manager.GetById(id);
+			if (shoe == null)
+			{
+				return NotFound("No such item, id: " + id);
+			}
+			else
+			{
+				return Ok(shoe);
+			}
+			
+		}
+
+		// POST api/<ShoesController>
+		[HttpPost]
+		public Shoe Post([FromBody] Shoe value)
+		{
+			return _manager.Add(value);
+		}
+
+		// PUT api/<ShoesController>/5
+		[HttpPut("{id}")]
+		public Shoe Put(int id, [FromBody] Shoe value)
+		{
+			return _manager.Update(id, value);
+		}
+
+		// DELETE api/<ShoesController>/5
+		[HttpDelete("{id}")]
+		public Shoe Delete(int id)
+		{
+			return _manager.Delete(id);
+		}
+	}
+}
---
+++ Managers/ShoesManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OblRestExcercise.Models;
+
+namespace OblRestExcercise.Managers
+{
+	public class ShoesManager
+	{
+		private static int _nextId = 1;
+		private static readonly List<Shoe> Data = new List<Shoe>
+		{
+			new Shoe {ID = _nextId++, Brand = "Adidas", Name = "NMD", Price = 749},
+			new Shoe {ID = _nextId++, Brand = "Puma", Name = "Smash", Price = 500},
+			new Shoe {ID = _nextId++, Brand = "Nike", Name = "AirForce 1", Price = 899}
+		};
+
+		public List<Shoe> GetAll()
+		{
+			return new List<Shoe>(Data);
+		}
+
+		public List<Shoe> GetWithContains(string substring)
+		{
+			List<Shoe> resultShoes = new List<Shoe>(Data);
+			if (substring != null)
+			{
+				resultShoes  = resultShoes.FindAll(shoe => shoe.Brand.Contains(substring))
+			;
+			}
+			return resultShoes;
+		}
+
+		public Shoe GetById(int id)
+		{
+			return Data.Find(item => item.ID == id);
+		}
+
+		public Shoe Add(Shoe newShoe)
+		{
+			newShoe.ID = _nextId++;
+			Data.Add(newShoe);
+			return newShoe;
+		}
+
+		public Shoe Delete(int id)
+		{
+			Shoe shoe = Data.Find(shoe1 => shoe1.ID == id);
+			if (shoe == null) return null;
+			Data.Remove(shoe);
+			return shoe;
+		}
+
+		public Shoe Update(int id, Shoe updates)
+		{
+			Shoe shoe = Data.Find(shoe1 => shoe1.ID == id);
+			if (shoe == null) return null;
+			shoe.Brand = updates.Brand;
+			shoe.Name = updates.Name;
+			shoe.Price = updates.Price;
+			return shoe;
+		}
+	}
+}
