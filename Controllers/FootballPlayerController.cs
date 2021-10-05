1939de8775c46f58c974092a7b9be0fc90b61d8e
benjahauge <Benjahauge@hotmail.com>
2021-10-05 14:01:41 +0200

    FootballPlayer added

---
+++ Controllers/FootballPlayerController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsharpExcercise;
+using Microsoft.AspNetCore.Http;
+using OblRestExcercise.Managers;
+using OblRestExcercise.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OblRestExcercise.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FootballPlayerController : ControllerBase
+	{
+		private readonly FootballPlayersManager _manager = new FootballPlayersManager();
+
+		// GET: api/<FootballPlayerController>
+		[HttpGet]
+		public IEnumerable<FootballPlayer> Get()
+		{
+			return _manager.GetAll();
+		}
+
+		// GET api/<FootballPlayerController>/5
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		// GET api/<ShoesController>/5
+		[HttpGet("{id}")]
+		public ActionResult<FootballPlayer> Get(int id)
+		{
+			FootballPlayer player = _manager.GetById(id);
+			if (player == null)
+			{
+				return NotFound("No such item, id: " + id);
+			}
+			else
+			{
+				return Ok(player);
+			}
+
+		}
+
+		// POST api/<FootballPlayerController>
+		[HttpPost]
+		public FootballPlayer Post([FromBody] FootballPlayer value)
+		{
+			return _manager.Add(value);
+		}
+
+		// PUT api/<FootballPlayerController>/5
+		[HttpPut("{id}")]
+		public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+		{
+			return _manager.Update(id, value);
+		}
+
+		// DELETE api/<FootballPlayerController>/5
+		[HttpDelete("{id}")]
+		public FootballPlayer Delete(int id)
+		{
+			return _manager.Delete(id);
+		}
+	}
+}
---
+++ Managers/FootballPlayersManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsharpExcercise;
+
+namespace OblRestExcercise.Managers
+{
+	public class FootballPlayersManager
+	{
+		private static int _nextId = 1;
+		private static readonly List<FootballPlayer> Data = new List<FootballPlayer>
+		{
+			new FootballPlayer {ID = _nextId++, Name = "Victor", Price = 10, ShirtNumber = 2},
+			new FootballPlayer {ID = _nextId++, Name = "Peter", Price = 500, ShirtNumber = 5}
+		};
+
+		public List<FootballPlayer> GetAll()
+		{
+			return new List<FootballPlayer>(Data);
+		}
+
+		public List<FootballPlayer> GetWithContains(string substring)
+		{
+			List<FootballPlayer> resultPlayers = new List<FootballPlayer>(Data);
+			if (substring != null)
+			{
+				resultPlayers = resultPlayers.FindAll(player => player.Name.Contains(substring))
+					;
+			}
+			return resultPlayers;
+		}
+
+		public FootballPlayer GetById(int id)
+		{
+			return Data.Find(item => item.ID == id);
+		}
+
+		public FootballPlayer Add(FootballPlayer newPlayer)
+		{
+			newPlayer.ID = _nextId++;
+			Data.Add(newPlayer);
+			return newPlayer;
+		}
+
+		public FootballPlayer Delete(int id)
+		{
+			FootballPlayer player = Data.Find(player1 => player1.ID == id);
+			if (player == null) return null;
+			Data.Remove(player);
+			return player;
+		}
+
+		public FootballPlayer Update(int id, FootballPlayer updates)
+		{
+			FootballPlayer player = Data.Find(player1 => player1.ID == id);
+			if (player == null) return null;
+			player.Name = updates.Name;
+			player.Price = updates.Price;
+			player.ShirtNumber = updates.ShirtNumber;
+			return player;
+		}
+	}
+}
